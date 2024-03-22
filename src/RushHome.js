6406cea718a57c44ae7503ee94cbd5c6cdeb1cee
YouVin <dbqls990909@naver.com>
2024-03-22 22:23:47 +0900

    RushHome tab 추가

--- src/RushHome.js
+++ src/RushHome.js
@@ -11,8 +11,12 @@ import {
   TableRow,
   Paper,
   Divider,
+  Tab,
+  Tabs,
 } from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import StopCircleIcon from "@mui/icons-material/StopCircle";
 import { Link } from "react-router-dom";
 import NotebookMenuBar from "./NotebookMenuBar";
 
@@ -20,6 +24,12 @@ const RushHome = () => {
   const [folderPath, setFolderPath] = useState("");
   const [fileList, setFileList] = useState([]);
   const fileInputRef = useRef(null);
+  const [value, setValue] = React.useState(0); //tabs
+
+  const handleChange = (event, newValue) => {
+    //tabs value change
+    setValue(newValue);
+  };
 
   const handleFolderFile = (event) => {
     const files = event.target.files;
@@ -73,60 +83,97 @@ const RushHome = () => {
           marginRight: "25px",
         }}
       ></Divider>
-      <Container style={{ flexGrow: 1, paddingTop: "10px" }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <FolderOpenIcon style={{ marginRight: "10px" }} />
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            File
-          </Typography>
-          <div>
+      <Container style={{ flexGrow: 1, paddingTop: "30px" }}>
+        <div>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="inherit"
+            indicatorColor="inherit"
+            sx={{
+              "& .MuiTabs-indicator": {
+                backgroundColor: "white", // 인디케이터 색상을 파랑으로 설정
+              },
+              alignItems: "center",
+              minHeight: 20,
+              height: 30,
+              minWidth: "auto",
+            }}
+          >
+            <Tab
+              icon={<FolderIcon sx={{ fontSize: 18 }} />}
+              iconPosition="start"
+              disableRipple
+              sx={{
+                borderBottom: "none", // 아래 border 제거
+                fontSize: 12,
+                fontWeight: "bold",
+              }}
+              label="Files" // 직접 label에 텍스트를 작성
+            />
+            <Tab
+              icon={<StopCircleIcon sx={{ fontSize: 20 }} />}
+              iconPosition="start"
+              disableRipple
+              sx={{
+                borderBottom: "none", // 아래 border 제거
+                fontWeight: "bold",
+                fontSize: 12,
+                color: "black",
+              }}
+              label="Run" // 직접 label에 텍스트를 작성
+            />
+          </Tabs>
+
+          <div style={{ paddingLeft: "5px", marginTop: "10px" }}>
             <Button
               variant="outlined"
-              onClick={createNewFile}
-              style={{ marginRight: "10px" }}
+              startIcon={<FolderOpenIcon />}
+              onClick={handleButtonClick}
             >
-              <Link
-                to="/nonamed"
-                target="_blank"
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
+              폴더 불러오기
+            </Button>
+            <Button variant="outlined" onClick={createNewFile}>
+              <Link to="/nonamed" target="_blank">
                 파일 생성하기
               </Link>
             </Button>
-            <Button variant="outlined" onClick={handleButtonClick}>
-              폴더 불러오기
-            </Button>
+            <input
+              type="file"
+              id="folderInput"
+              ref={fileInputRef}
+              accept=""
+              webkitdirectory="true"
+              directory="true"
+              onChange={handleFolderFile}
+              style={{
+                display: "none",
+              }}
+            />
+            <p>선택한 폴더 경로: {folderPath}</p>
           </div>
-        </div>
-        <div
-          style={{ display: "flex", alignItems: "center", marginTop: "10px" }}
-        >
-          <Typography variant="body1" style={{ flexGrow: 1 }}>
-            내가 따로 텍스트를 적어둘 예정
-          </Typography>
-          <Typography variant="body1">{folderPath}</Typography>
-        </div>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <TableContainer component={Paper}>
-            <Table aria-label="file table">
-              <TableHead>
-                <TableRow>
-                  <TableCell style={{ width: "43%" }}>
-                    <Typography variant="subtitle1">이름</Typography>
-                  </TableCell>
-                  <TableCell style={{ width: "24.5%" }}>
-                    <Typography variant="subtitle1">날짜</Typography>
-                  </TableCell>
-                  <TableCell style={{ width: "15%" }}>
-                    <Typography variant="subtitle1">유형</Typography>
-                  </TableCell>
-                  <TableCell style={{ width: "20%" }}>
-                    <Typography variant="subtitle1">크기</Typography>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-            </Table>
-          </TableContainer>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <TableContainer component={Paper}>
+              <Table aria-label="file table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ width: "43%" }}>
+                      <Typography variant="subtitle1">이름</Typography>
+                    </TableCell>
+                    <TableCell style={{ width: "24.5%" }}>
+                      <Typography variant="subtitle1">날짜</Typography>
+                    </TableCell>
+                    <TableCell style={{ width: "15%" }}>
+                      <Typography variant="subtitle1">유형</Typography>
+                    </TableCell>
+                    <TableCell style={{ width: "20%" }}>
+                      <Typography variant="subtitle1">크기</Typography>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+          </div>
           <div style={{ maxHeight: "65vh", overflowY: "auto" }}>
             <TableContainer component={Paper}>
               <Table aria-label="file table">
