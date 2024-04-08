c98d81ac7bf76495e6b5a406c8ff09dd443b3e23
YouVin <dbqls990909@naver.com>
2024-04-08 21:47:03 +0900

    historypage 수정

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -6,7 +6,10 @@ import {
   ListItemText,
   Typography,
   Divider,
+  IconButton,
 } from "@mui/material";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const HistoryPage = () => {
   const [savedHistory, setSavedHistory] = useState([]);
@@ -71,48 +74,59 @@ const HistoryPage = () => {
     return `${years} years ago`;
   };
 
+  const handleDeleteItem = (index) => {
+    const updatedHistory = [...savedHistory];
+    updatedHistory.splice(index, 1);
+    localStorage.setItem("savedHistory", JSON.stringify(updatedHistory));
+    setSavedHistory(updatedHistory);
+  };
+
   return (
     <div>
       <Typography variant="h5" gutterBottom style={{ marginTop: 20 }}>
         History
       </Typography>
-      <Divider sx={{ backgroundColor: "rgba(0, 0, 51, 1)", marginTop: 3 }} />
-      <Paper elevation={0} sx={{ boxShadow: "none", marginTop: 3 }}>
-        {Object.entries(groupedHistory)
-          .reverse()
-          .map(([date, historyItems]) => (
-            <div key={date}>
-              <Typography variant="subtitle1" style={{ marginTop: 10 }}>
-                {date}
-              </Typography>
-              <List component="div">
-                {historyItems
-                  .slice()
-                  .reverse()
-                  .map((item, index) => (
-                    <ListItem
-                      key={index}
-                      sx={{
-                        border: "1px solid rgba(0, 0, 51, 0.3)",
-                        borderRadius: 1,
-                        borderTop: "1px solid rgba(0, 0, 51, 0.3)",
-                        borderBottom:
-                          index === historyItems.length - 1
-                            ? "1px solid rgba(0, 0, 51, 0.3)"
-                            : "none",
-                      }}
-                    >
-                      <ListItemText
-                        primary={`Update ${item.title}`}
-                        secondary={`create ${
-                          item.cellItemCount ? item.cellItemCount : 0
-                        } cellItems •  ${formatTimeAgo(item.saveTime)}`}
-                      />
-                    </ListItem>
-                  ))}
-              </List>
-            </div>
-          ))}
+      <Divider sx={{ backgroundColor: "rgba(0, 0, 51, 1)", marginTop: 2 }} />
+      <Paper elevation={0} sx={{ boxShadow: "none", marginTop: 5 }}>
+        {Object.entries(groupedHistory).map(([date, historyItems]) => (
+          <div key={date} style={{ marginTop: 10 }}>
+            <Typography variant="button" style={{ fontSize: 15 }}>
+              <CalendarViewDayIcon style={{ marginRight: 15 }} />
+              {date}
+            </Typography>
+            <List component="div">
+              {historyItems.map((item, index) => (
+                <ListItem
+                  key={index}
+                  sx={{
+                    border: "1px solid rgba(0, 0, 51, 0.3)",
+                    borderRadius: 1,
+                    borderTop: "1px solid rgba(0, 0, 51, 0.3)",
+                    borderBottom:
+                      index === historyItems.length - 1
+                        ? "1px solid rgba(0, 0, 51, 0.3)"
+                        : "none",
+                  }}
+                >
+                  <ListItemText
+                    primary={`[UPDATE] • ${item.title}`}
+                    primaryTypographyProps={{
+                      fontSize: 16,
+                      fontWeight: "medium",
+                      letterSpacing: 0,
+                    }}
+                    secondary={`create ${
+                      item.cellItemCount ? item.cellItemCount : 0
+                    } cellItems •  ${formatTimeAgo(item.saveTime)}`}
+                  />
+                  <IconButton onClick={() => handleDeleteItem(index)}>
+                    <ClearIcon />
+                  </IconButton>
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        ))}
       </Paper>
     </div>
   );
